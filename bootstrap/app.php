6337f50b4e716d7cc419f6150d392cb265b10bed
Julio Notodiprodyo <njulioiyoo@gmail.com>
2025-09-27 00:04:11 +0700

    middleware cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->trustProxies(at: '*');
 
         $middleware->web(append: [
             HandleAppearance::class,
