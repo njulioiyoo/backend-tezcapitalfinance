910d3784a67c3edf55d056ccdd6013d4f7d13226
Julio Notodiprodyo <njulioiyoo@gmail.com>
2025-09-26 23:59:26 +0700

    fix: fixed cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://tezapi.dev.tezcapitalfinance.co.id'],
 
     'allowed_origins_patterns' => [],
 
