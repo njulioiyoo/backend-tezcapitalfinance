9ed96dabfe63a195b28fafe6302202ed6efc8fd4
Julio Notodiprodyo <njulioiyoo@gmail.com>
2025-09-29 21:43:51 +0700

    fix: fixed api config url

--- app/Http/Controllers/Api/ConfigurationController.php
+++ app/Http/Controllers/Api/ConfigurationController.php
@@ -264,7 +264,7 @@ class ConfigurationController extends Controller
                     'sort_order' => $service->sort_order,
                     'created_at' => $service->created_at?->toISOString(),
                     'updated_at' => $service->updated_at?->toISOString(),
-                    'url' => url('/services/' . $service->id)
+                    'url' => config('app.url') . '/services/' . $service->id
                 ];
             })->toArray()
         ];
